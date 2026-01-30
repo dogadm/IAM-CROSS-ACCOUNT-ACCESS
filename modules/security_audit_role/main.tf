dd8d25cb2f4c32a9733a0276493ede6af9bee2a0
Dolapo Adeola <125673226+dogadm@users.noreply.github.com>
2026-01-30 19:43:24 +0000

    Implement IAM role and policies for security audit

--- modules/security_audit_role/main.tf
+++ modules/security_audit_role/main.tf
@@ -1,1 +1,37 @@
-# Placeholder – implementation to follow
+data "aws_iam_policy_document" "trust" {
+  statement {
+    sid     = "AllowAuditOperatorWithExternalId"
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [var.audit_operator_role_arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "sts:ExternalId"
+      values   = [var.external_id]
+    }
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name                 = var.role_name
+  assume_role_policy   = data.aws_iam_policy_document.trust.json
+  max_session_duration = var.max_session_duration
+}
+
+resource "aws_iam_role_policy_attachment" "securityaudit" {
+  count      = var.attach_securityaudit ? 1 : 0
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
+}
+
+resource "aws_iam_role_policy_attachment" "viewonly" {
+  count      = var.attach_viewonly ? 1 : 0
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/ViewOnlyAccess"
+}
+
