2b51832b3e084efc887b3c59d410b9d711dd3870
Dolapo Adeola <125673226+dogadm@users.noreply.github.com>
2026-01-30 19:42:56 +0000

    Add variables for SecurityAuditRole module
    Define variables for security audit role configuration.

--- modules/security_audit_role/variables.tf
+++ modules/security_audit_role/variables.tf
@@ -1,1 +1,29 @@
-# Placeholder – implementation to follow
+variable "role_name" {
+  type    = string
+  default = "SecurityAuditRole"
+}
+
+variable "audit_operator_role_arn" {
+  type        = string
+  description = "ARN of the Audit account operator role that can assume this role"
+}
+
+variable "external_id" {
+  type        = string
+  description = "ExternalId required to assume the role"
+}
+
+variable "max_session_duration" {
+  type    = number
+  default = 3600
+}
+
+variable "attach_viewonly" {
+  type    = bool
+  default = true
+}
+
+variable "attach_securityaudit" {
+  type    = bool
+  default = true
+}
