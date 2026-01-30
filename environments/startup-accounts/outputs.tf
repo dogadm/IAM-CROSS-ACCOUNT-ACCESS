56b52beb495643ef89d836c6e483916eb8aa187e
Dolapo Adeola <125673226+dogadm@users.noreply.github.com>
2026-01-30 19:45:44 +0000

    Add security audit role ARNs to outputs.tf

--- environments/startup-accounts/outputs.tf
+++ environments/startup-accounts/outputs.tf
@@ -1,1 +1,4 @@
-# Placeholder – implementation to follow
+output "dev_security_audit_role_arn"    { value = module.security_audit_dev.role_arn }
+output "uat_security_audit_role_arn"    { value = module.security_audit_uat.role_arn }
+output "prod2_security_audit_role_arn"  { value = module.security_audit_prod2.role_arn }
+output "gaming_security_audit_role_arn" { value = module.security_audit_gaming.role_arn }
